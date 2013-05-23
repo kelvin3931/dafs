53fd9160106502c7a9453bccabeb7d2d94854483
Jerry Wu <jerry.wu@quantatw.com>
2013-05-23 10:13:59 +0800

    use sprintf instead strcpy and strncat.

---
+++ src/da_sql/sql.h
@@ -0,0 +1,23 @@
+#ifndef _SQL_H_
+#define _SQL_H_
+
+#define MAX_LEN 1024
+//#define CLOUD_PATH "https://192.168.88.14:8080/abc/"
+//#define ARCHIVED 0
+#define DBPATH "/home/jerry/hsm_fuse/src/sql.db"
+
+#define FULL_PATH "/fullpath"
+#define FILENAME "123.txt"
+#define PARENT "/parent"
+
+void show_file_stat(struct stat *si);
+sqlite3 *init_db(sqlite3 *db, char *filename);
+int insert_rec(sqlite3 *db, char *fpath, struct stat* statbuf, char *path);
+int insert_db(char *fpath, struct stat* statbuf);
+int remove_rec(sqlite3 *db, char *path);
+int update_rec(sqlite3 *db, char *fpath, struct stat* statbuf, char *path);
+int update_rec_rename(sqlite3 *db, char *fpath, struct stat* statbuf,
+                      char *fnewpath, char *path, char *newpath);
+int get_rec(sqlite3 *db, char *fpath, struct stat* statbuf);
+
+#endif
