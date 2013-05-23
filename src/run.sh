55b7cb2997d4870e82605fc2dddefa07f079c7fe
Jerry Wu <jerry.wu@quantatw.com>
2013-05-23 10:05:30 +0800

    Add run.sh script.

---
+++ src/run.sh
@@ -0,0 +1,6 @@
+#!/bin/bash -x
+
+sudo /usr/gnu/bin/fusermount -u ../example/mountdir;
+make clean; make oi;
+sudo ./bbfs ../example/rootdir ../example/mountdir;
+swift -A https://192.168.88.14:8080/auth/v1.0 -U test:tester -K testing list abc;
